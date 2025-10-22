bb74f569e75a042fe298399167343e87952583ce
jiangscode <jiang.zhke.m@gmail.com>
2025-10-22 21:23:11 +0400

    basic caculations for drag force
    small basic drag force calculations

---
+++ basic calculations.py
@@ -0,0 +1,25 @@
+#basic velocity
+#drag force (if object moving)
+#lift force  (in wings)
+#pressure at a point aka bernoolli's princaple 
+#reynolds number
+#KE of air
+# flow of 2d grid
+
+import math
+
+def menu_config():
+    velocity = float(input("enter air velocity:"))
+    object_area = float(input("enter object area:"))
+    dragCF = float(input("enter drag coefficient:"))
+    ad = float(input("enter air density:"))
+    return velocity, ad, dragCF, object_area
+
+def dragforce(v, ad, dCF, oa):
+    dragforce = (v*v)*ad*dCF*oa
+    print("df =", round(dragforce, 2))
+    return dragforce
+
+v, ad, dCF, oa = menu_config()
+drag = dragforce(v, ad, dCF, oa)
+
